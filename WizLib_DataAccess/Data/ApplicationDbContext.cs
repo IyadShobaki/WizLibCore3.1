f98e07904b4ba30f46d3203e76a3811027ce424d
IyadShobaki <iyadshobaki82@gmail.com>
2020-11-05 15:05:21 -0500

    Configure Entity Framework
    - Install NuGet packges :
        "Microsoft.EntityFrameworkCore"
        "Microsoft.EntityFramework.SqlServer"
    - Add connection string to appsettings.json
    - Add ApplicationDbContext class inside WizLib_DataAccess - Data foder and inherit from DbContext.
    - Inside Startup.cs add DbContext service.

---
+++ WizLib_DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WizLib_DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :
+            base(options)
+        {
+        }
+
+
+    }
+}
